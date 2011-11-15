42cfe719f3dbe2f222e458f09a8104c33d4b74e3
CeBe <mail@cebe.cc>
2011-11-15 15:46:38 +0100

    created example php class

---
+++ example-code/ExampleClass.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * This is the short description of file docbloc for ExampleClass
+ * 
+ * Sed arcu tortor, aliquam eget posuere sed, volutpat quis ipsum. 
+ * Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+ * Suspendisse potenti.
+ * 
+ * @author CeBe <mail@cebe.cc>
+ */
+
+
+/**
+ * This is the short description of the ExampleClass
+ *
+ * Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam at elit magna. Nulla leo ipsum, 
+ * mattis at mattis imperdiet, rutrum at turpis. 
+ * Maecenas vulputate sodales metus. Maecenas nec odio dui, ac euismod felis.
+ * Pellentesque feugiat blandit sapien ac tincidunt. In velit leo, condimentum id ornare vel, 
+ * congue ac erat. 
+ * 
+ * <ul>
+ *   <li>Vivamus molestie adipiscing</li>
+ *   <li>lacus id dapibus.</li>
+ *   <li>Donec sit amet purus augue</li>
+ * </ul>
+ *
+ * @package example.class
+ */
+class ExampleClass extends AnotherClass implements Countable, Iterator
+{
+
+
+
+}
